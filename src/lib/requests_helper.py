f9893b0ec4ed348883764b047f6821dfc32a0c96
rrhuffy <rrhuffy@gmail.com>
2022-08-11 20:28:11 +0200

    use custom user agent in requests_helper

--- src/lib/requests_helper.py
+++ src/lib/requests_helper.py
@@ -3,13 +3,17 @@ import os
 import re
 
 import requests
+import subprocess
+
+USER_AGENT_FIREFOX = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'
+USER_AGENT_CURL = 'curl/7.54.1'
+USER_AGENT_HYBRISTOOLS = 'hybristools'
 
 
 def get_session():
     requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
     session = requests.sessions.Session()
-    # pretend_to_be_curl(session)
-    # pretend_to_be_firefox(session)
+    set_user_agent(session)
     proxy = os.environ.get('HTTPS_PROXY')
     if proxy:
         session.proxies = {
@@ -32,17 +36,30 @@ def get_session_with_basic_http_auth_and_cleaned_address(address):
     return session, address
 
 
-def pretend_to_be_curl(session):
-    session.headers['User-Agent'] = 'curl/7.54.1'
-
-
-def pretend_to_be_firefox(session):
-    session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'
-
-
 def disable_proxy():
     os.environ['NO_PROXY'] = '*'
 
 
 def enable_proxy():
     os.environ.pop('NO_PROXY', None)
+
+
+def set_user_agent(session, custom_user_agent=None):
+    if custom_user_agent:
+        session.headers['User-Agent'] = custom_user_agent
+    else:
+        try:
+            # try to get user from git global config
+            postfix = subprocess.check_output('git config --global --get user.email'.split(' ')).decode().strip()
+        except (subprocess.CalledProcessError, FileNotFoundError):
+            import getpass
+            user = getpass.getuser()
+            import socket
+            hostname = socket.gethostname()
+            postfix = f'{user}@{hostname}'
+
+        user_agent = USER_AGENT_HYBRISTOOLS  # or USER_AGENT_CURL or USER_AGENT_FIREFOX
+        if postfix:
+            user_agent += f'-{postfix}'
+
+        session.headers['User-Agent'] = user_agent
